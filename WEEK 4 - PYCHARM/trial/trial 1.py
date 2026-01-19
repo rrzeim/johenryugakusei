a0deb045a9fa54e3d6352156d2859b44d1c059c8
Johen Ryugakusei D. De Leon <b5mtdtbkwj@privaterelay.appleid.com>
2026-01-19 17:34:46 +0800

    Delete WEEK 4 - PYCHARM directory

--- WEEK 4 - PYCHARM/trial/trial 1.py
+++
@@ -1,7 +0,0 @@
-print("Hello World Double Quote")
-print('Hello World Single Quote')
-print('Ryugakusei says "i love programming!!!"')
-print("Ryugakusei says 'i love math!!!'")
-print("Ryugakusei says \"i love physics!!!\"")
-
-print(input("Enter your PIN number : "))

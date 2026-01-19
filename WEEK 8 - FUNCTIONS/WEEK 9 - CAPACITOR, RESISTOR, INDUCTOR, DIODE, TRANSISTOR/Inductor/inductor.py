404a815cc7f475149e03bff7cc4feb3203f79b3b
Johen Ryugakusei D. De Leon <b5mtdtbkwj@privaterelay.appleid.com>
2026-01-19 17:35:35 +0800

    Delete WEEK 8 - FUNCTIONS directory

--- WEEK 8 - FUNCTIONS/WEEK 9 - CAPACITOR, RESISTOR, INDUCTOR, DIODE, TRANSISTOR/Inductor/inductor.py
+++
@@ -1,5 +0,0 @@
-def get_inductor_value_of_material(relative_permeability, area, turns, length):
-    mu_0 = 4 * 3.141592653589793 * 1e-7  # H/m, permeability of free space
-    inductance = (mu_0 * relative_permeability * turns**2 * area) / length
-    print(f"Inductor Value: {inductance} H")
-    return inductance
